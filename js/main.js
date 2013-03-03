b89950941e125ab7814e39e9ae64147971ba3398
James Chow <james.chow@mail.adsl4less.com>
2013-03-03 23:28:49 +0000

    added query string process

--- js/main.js
+++ js/main.js
@@ -3,9 +3,13 @@
 
     var _holder = {};
 
-    function updateElement(){
+    function onBChain(event){
+        updateElement.call(this,this.getComposed()());
+    }
+
+    function updateElement(val){
         var element = document.getElementById(_holder[this].selector);
-        element.innerHTML = this.getComposed()();
+        element.innerHTML = val
     }
 
     window.tQuery = tQuery = function(selector,context){
@@ -24,7 +28,8 @@
                 chains:[],
                 attachChain:function(chain){
                     this.chains.push(chain);
-                    chain.on('all',updateElement,chain);
+                    chain.attachListeners();
+                    chain.on('all',onBChain,chain);
                 }
             }
             return this;
@@ -33,9 +38,8 @@
             _holder[this].chainHandler.attachChain(chain);
             return this;
         },
-        forceBind:function(){
-            updateElement();
-            return this;
+        setter:function(){
+            return _.bind(updateElement,this);
         }
     }
 
@@ -44,27 +48,80 @@
 
 (function(window,document,Backbone,_,undefined){
 
-    var _chain = {}
+    var _chain = {};
 
-    var processes = {
-        property:function(property,model) {
-            return (model !== undefined && model instanceof Backbone.Model) ? model.get(property) : undefined;
+    var chainStorage = {
+        PropertyWatcher:function(property,model) {
+            if (model !== undefined && model instanceof Backbone.Model){
+                //backbone model
+                return model.get(property)
+            }
+            else if (model !== undefined && _.has(model,property))
+            {
+                //array item
+                return model[property];
+            }
+
+            return undefined;
         },
-        index:function(index,collection){
-            return (collection !== undefined && collection instanceof Backbone.Collection) ? collection.at(index) : undefined;
+        IndexWatcher:function(index,collection){
+            if (collection !== undefined && collection instanceof Backbone.Collection){
+                //backbone collection
+                return collection.at(index)
+            }
+            else if (collection !== undefined && _.isArray(collection))
+            {
+                //array item
+                return collection[index];
+            }
+
+            return undefined;
         },
-        collection:function(collection,item){
+        CollectionWatcher:function(collection,item){
             return collection;
         },
-        model:function(model,item){
+        ModelWatcher:function(model,item){
             return model;
         },
-        transform:function(func,item){
+        TransformWatcher:function(func,item){
             return (item !== undefined && _.isFunction(func)) ? func(item) : undefined;
+        },
+        EmptyTransformWatcher:function(emptyvalue,item){
+            return (item !== undefined) ? item : emptyvalue;
         }
     }
 
-    var chainHandler = {
+    var PropertyWatcher = function(property){
+        this.property = property;
+        this.partial = _.partial(chainStorage.PropertyWatcher,property);
+    }
+
+    var IndexWatcher = function(index){
+        this.index = index;
+        this.partial = _.partial(chainStorage.IndexWatcher,index);
+    }
+
+    var CollectionWatcher = function(collection){
+        this.collection = collection;
+        this.partial = _.partial(chainStorage.CollectionWatcher,collection);
+    }
+
+    var ModelWatcher = function(model){
+        this.model = model;
+        this.partial = _.partial(chainStorage.ModelWatcher,model);
+    }
+
+    var TransformWatcher = function(transform){
+        this.transform = transform;
+        this.partial = _.partial(chainStorage.TransformWatcher,transform);
+    }
+
+    var EmptyTransformWatcher = function(emptyValue){
+        this.emptyValue = emptyValue;
+        this.partial = _.partial(chainStorage.EmptyTransformWatcher,emptyValue);
+    }
+
+    var eventHandler = {
         listenedModels:[],
         onCollectionEvent: function(event){
             this.trigger(event);
@@ -82,10 +139,30 @@
                 this.listenedModels.push(model);
                 model.on('all',this.onCollectionEvent,context);
             }
+        },
+        attachListeners:function(list,context){
+            var self = this;
+            _.each(list,function(item){
+                if (item instanceof CollectionWatcher)
+                {
+                    self.attachCollectionListener(item.collection,context);
+                }
+                if (item instanceof ModelWatcher)
+                {
+                    self.attachModelListener(item.model,context);
+                }
+            });
+        },
+        detachListeners:function(){
+            var self = this;
+            _.each(self.listenedModels,function(item){
+                item.off();
+            });
         }
     }
 
-    window.bChain = bChain = function(eventedModel,context){
+    window.bChain = bChain = function(eventedModel){
+        var context = this;
         return new BChain(eventedModel,context);
     }
 
@@ -98,54 +175,93 @@
     BChain.fn = BChain.prototype = {
         init:function (eventedModel, context){
 
+            //making this an observable
             _.extend(this, Backbone.Events);
 
-            //see if we are starting with a collection of model
 
             if (eventedModel instanceof Backbone.Collection){
+                //see if we are starting with a collection
                 this.collection(eventedModel);
             }
             else if (eventedModel instanceof Backbone.Model){
+                //see if we are starting with a model
                 this.model(eventedModel);
+            }
+            else if (_.isString(eventedModel)){
+                //see if we are starting with a selector query
+                this.query(eventedModel,context);
             }
 
             return this;
         },
         collection:function(collection){
             if (collection instanceof Backbone.Collection){
-                _chain[this].unshift(_.partial(processes.collection, collection));
-
-                chainHandler.attachCollectionListener(collection,this);
+                _chain[this].unshift(new CollectionWatcher(collection));
             }
             return this;
         },
         model:function(model){
             if (model instanceof Backbone.Model){
-                _chain[this].unshift(_.partial(processes.model, model));
-                chainHandler.attachModelListener(model,this);
+                _chain[this].unshift(new ModelWatcher(model));
             }
             return this;
         },
         get:function(property){
             if (_.isString(property)){
-                _chain[this].unshift(_.partial(processes.property, property));
+                _chain[this].unshift(new PropertyWatcher(property));
             }
             return this;
         },
         at:function(index){
             if (_.isNumber(index)){
-                _chain[this].unshift(_.partial(processes.index, index));
+                _chain[this].unshift(new IndexWatcher(index));
             }
             return this;
         },
         transform:function(transform){
             if (_.isFunction(transform)){
-                _chain[this].unshift(_.partial(processes.transform, transform));
+                _chain[this].unshift(new TransformWatcher(transform));
             }
             return this;
         },
+        brokenChainEndValue:function(emptyValue){
+            _chain[this].unshift(new EmptyTransformWatcher(emptyValue))
+            return this;
+        },
         getComposed:function(){
-            return _.compose.apply(this,_chain[this]);
+            var partials = _.pluck(_chain[this], 'partial');
+
+            return _.compose.apply(this,partials);
+        },
+        attachListeners:function(){
+            eventHandler.attachListeners(_chain[this],this);
+            return this;
+        },
+        query:function(query,context){
+            var list = query.split(".");
+            var self = this;
+            _.each(list,function(item,idx){
+                var variableName = item;
+                var atSymbol = /[\d+]/.exec(item);
+                if (!_.isNull(atSymbol)){
+                    variableName  = item.substr(0,atSymbol.index-1);
+                }
+
+                if ( idx == 0 )
+                {
+                    self.collection(context[variableName]);
+                }
+                else
+                {
+                    self.get(variableName);
+                }
+
+                if (!_.isNull(atSymbol)){
+                    var index = parseInt(atSymbol.toString());
+                    self.at(index);
+                }
+            });
+            return this;
         }
     }
 
@@ -153,14 +269,14 @@
 
 var library = new Backbone.Collection([{name:'feersum',author:'banks'}]);
 
-var authored = bChain(library).at(2).get("author").getComposed();
-bChain(library).at(2).get('name').transform(function(item){return item+authored()});
-
-tQuery('output').addBChain(bChain(library).at(2).get('name'));
+//var authored = bChain(library).at(2).get("author").brokenChainEndValue('').getComposed();
+//bChain(library).at(2).get('name').transform(function(item){return item+" "+authored()}).on('all');
+//bChain().query('library[2].addresses[1].street',this);
+tQuery('output').addBChain(bChain('library[2].addresses[0].street').brokenChainEndValue('cannot find'));
 
 setTimeout(function(){
     library.add({name:'use of weapons',author:'banks'});
-},3000);
+},1000);
 setTimeout(function(){
-    library.add({name:'state of the art',author:'banks'});
-},6000);
+    library.add({name:'state of the art',author:'banks',addresses:[{street:'unicorn road'}]});
+},2000);
